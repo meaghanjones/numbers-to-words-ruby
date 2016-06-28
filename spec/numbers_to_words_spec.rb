c9fe8f7bb3f6a128c9bfdf075d404f998f2496cc
Brett New and Meaghan Jones <brett@>
2016-06-28 15:27:45 -0700

    add ability to input up to 1 million and translate into words

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -9,18 +9,24 @@ describe('Fixnum#numbers_to_words') do
     expect(10.numbers_to_words()).to(eq("ten"))
   end
   it('returns a word for special cases of combinations of the tens and ones place') do
-    expect(99.numbers_to_words()).to(eq("ninety-nine"))
+    expect(99.numbers_to_words()).to(eq("ninety nine"))
   end
   it('returns a word for the hundreds place') do
     expect(200.numbers_to_words()).to(eq("two hundred "))
   end
   it('returns a word for the thousands place') do
-    expect(1234.numbers_to_words()).to(eq("one thousand two hundred thirty-four"))
+    expect(1234.numbers_to_words()).to(eq("one thousand two hundred thirty four"))
   end
   it('returns a word for the ten thousands place if the number is equal to or larger than 20,000') do
-    expect(32234.numbers_to_words()).to(eq("thirty-two thousand two hundred thirty-four"))
+    expect(32234.numbers_to_words()).to(eq("thirty two thousand two hundred thirty four"))
   end
   it('returns a word for the ten thousands place if the number is less than 20,000 but greater than 9,999') do
-    expect(19234.numbers_to_words()).to(eq("nineteen thousand two hundred thirty-four"))
+    expect(19234.numbers_to_words()).to(eq("nineteen thousand two hundred thirty four"))
+  end
+  it('returns a word for the hundred thousands place') do
+  expect(143000.numbers_to_words()).to(eq("one hundred forty three thousand "))
+  end
+  it('returns a word for the millions place') do
+    expect(1230000.numbers_to_words()).to(eq("one million two hundred thirty thousand "))
   end
 end
