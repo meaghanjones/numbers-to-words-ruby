fbf858dbbd630c6b1d5c44b0d04e5a2168b13982
Brett New and Meaghan Jones <brett@>
2016-06-28 14:14:32 -0700

    create ruby code to translate numbers into words; have reached the ten thousand spot. create rspec to test each condition.

---
+++ lib/numbers_to_words.rb
@@ -0,0 +1,34 @@
+require('rspec')
+
+class Fixnum
+  define_method(:numbers_to_words) do
+    input_number = self
+    number = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
+    word = ""
+    if !number.include?(self)
+      if input_number./(10000).>=(2)
+        ten_thousand_number = input_number./(10000).floor.*(10)
+        word.concat(number.fetch(ten_thousand_number)).concat("-")
+        input_number = input_number.%(10000)
+      end
+      if input_number./(1000).>(0)
+        thousand_number = input_number./(1000).floor()
+        word.concat(number.fetch(thousand_number)).concat(" thousand ")
+        input_number = input_number.%(1000)
+      end
+      if input_number./(100).>(0)
+        hundred_number = input_number./(100).floor()
+        word.concat(number.fetch(hundred_number)).concat(" hundred ")
+        input_number=input_number.%(100)
+      end
+      if input_number./(10).>(0)
+        ten_number = input_number./(10).floor.*(10)
+        one_number = input_number.%10
+        word.concat(number.fetch(ten_number)).concat("-").concat(number.fetch(one_number))
+      end
+    else
+    word.concat(number.fetch(input_number))
+    end
+  word
+  end
+end
---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,26 @@
+require('rspec')
+require('numbers_to_words.rb')
+
+describe('Fixnum#numbers_to_words') do
+  it('returns a word for numbers 1-9') do
+    expect(1.numbers_to_words()).to(eq("one"))
+  end
+  it('returns a words for number 10') do
+    expect(10.numbers_to_words()).to(eq("ten"))
+  end
+  it('returns a word for special cases of combinations of the tens and ones place') do
+    expect(99.numbers_to_words()).to(eq("ninety-nine"))
+  end
+  it('returns a word for the hundreds place') do
+    expect(200.numbers_to_words()).to(eq("two hundred "))
+  end
+  it('returns a word for the thousands place') do
+    expect(1234.numbers_to_words()).to(eq("one thousand two hundred thirty-four"))
+  end
+  it('returns a word for the ten thousands place if the number is equal to or larger than 20,000') do
+    expect(32234.numbers_to_words()).to(eq("thirty-two thousand two hundred thirty-four"))
+  end
+  it('returns a word for the ten thousands place if the number is less than 20,000 but greater than 9,999') do
+    expect(19234.numbers_to_words()).to(eq("nineteen thousand two hundred thirty-four"))
+  end
+end
